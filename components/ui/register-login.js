51b9bf62913260af6c20c9300a56588977296fa2
Anna Nagy <annacnagy@gmail.com>
2016-04-21 23:37:00 -0400

    improved log out, working on registration vs login

--- components/ui/register-login.js
+++ components/ui/register-login.js
@@ -3,36 +3,69 @@ if (Meteor.isClient) {
     // Registration and User Creation
 
     Template.register.events({
+        'click #showLoginLink' : function(){
+            console.log("show login");
+            Session.set('showRegister',false);
+        },
         'submit form': function(event) {
             event.preventDefault();
-            console.log("registration form submitted");
-            var userVar: event.target.registerUser.value;
-            var password: event.target.registerPassword.value;
-            
-            var isValidPassword = function(val) {
-             return val.length; 
-            }
+            var emailVar = $('[name=registerEmail]').val();
+            var passwordVar = $('[name=registerPassword]').val();
+            Accounts.createUser({
+                email: emailVar,
+                password: passwordVar
+            });
+            console.log('registration form submitted');
+        }
+    });
 
-            if (isValidPassword(userPassword)) {
-                    Accounts.createUser ({
-                    username: userVar,
-                    password: passwordVar,
-                });
-            }
+
+    Template.register.helpers({
+        showRegisterTemplate : function () {
+            return Session.get('showRegister')
         }
     });
 
     // Logging In
 
     Template.login.events({
+        'click #showRegisterLink': function() {
+            console.log("show register");
+            Session.set('showRegister',true);
+        },
         'submit form': function(event) {
             event.preventDefault();
-            console.log("login form submitted");
-            var userVar: event.target.loginUser.value;
-            var passwordVar: event.target.loginPassword.value;
-            Meteor.loginWithPassword(userVar, passwordVar);
+            console.log('login form submitted');
+            var emailVar = $('[name=loginEmail]').val();
+            var passwordVar = $('[name=loginPassword]').val()
+            Meteor.loginWithPassword(emailVar, passwordVar, function(err){
+                if (err) {
+                    $('#login-error').removeClass('hidden')
+                } else {
+                    return false;
+                }
+            });
         }
     });
+
+        Template.login.helpers({
+        showRegisterTemplate : function () {
+            return Session.get('showRegisterTemplate')
+        }
+    });
+
+
+    // Logging Out
+
+    Template.logout.events({
+        'click .logout': function(event){
+            event.preventDefault();
+            Meteor.logout();
+            Router.go('login')
+        }
+    });
+
+    // Toggling User
 }
 
 
